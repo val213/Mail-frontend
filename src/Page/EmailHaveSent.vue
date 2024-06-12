56bd5e9e7ff64412c0da72b2994473c37d4cdc44
val213 <1042594899@qq.com>
2024-06-12 09:46:08 +0800

    fix bug in merge conflict.

--- src/Page/EmailHaveSent.vue
+++ src/Page/EmailHaveSent.vue
@@ -18,7 +18,7 @@
         fetchUserInfo() {
             // 假设有一个方法来获取用户信息
             // 这里简化为直接赋值
-            this.userId = '6'; // 假设的用户ID
+            this.userId = '7'; // 假设的用户ID
         }
     },
     created() {
